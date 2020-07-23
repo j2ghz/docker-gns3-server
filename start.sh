4ce8c9094ccf29ef39d55009fbb753e89ffe00a0
Ľuboš Lendáč <Arcanum417@users.noreply.github.com>
2020-07-23 15:48:03 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,11 +10,24 @@ fi
 brctl addbr virbr0
 ip link set dev virbr0 up
 if [ "${BRIDGE_ADDRESS}x" == "x" ]; then
-  BRIDGE_ADDRESS=172.21.1.1/24
+  BRIDGE_ADDRESS=172.84.9.1/24
 fi
-ip ad add ${BRIDGE_ADDRESS} dev virbr0
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
-dnsmasq -i virbr0 -z -h --dhcp-range=172.21.1.10,172.21.1.250,4h
+if [ "${DHCP_START}x" == "x" ]; then
+  DHCP_START=172.84.9.10
+fi
+
+if [ "${DHCP_END}x" == "x" ]; then
+  DHCP_END=172.84.9.100
+fi
+
+if [ "${BRIDGE_INTERFACE}x" == "x" ]; then
+  BRIDGE_INTERFACE=eth0
+fi
+
+ip ad add ${BRIDGE_ADDRESS} dev virbr0
+iptables -t nat -A POSTROUTING -o ${BRIDGE_INTERFACE} -j MASQUERADE
+
+dnsmasq -i virbr0 -z -h --dhcp-range=${DHCP_START},${DHCP_END},4h
 dockerd --storage-driver=vfs --data-root=/data/docker/ &
 gns3server -A --config /data/config.ini
